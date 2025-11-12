19918ef2eb7b42b66e5dd58b2ba8b3a024d7b8f1
Nathan-Rogers-Offical <148917066+Nathan-Rogers-Offical@users.noreply.github.com>
2025-11-12 09:48:04 -0700

    Idea for Project Homepage

---
+++ Project Homepage/node.js
@@ -0,0 +1,16 @@
+let mysql = require('mysql');
+
+let con = mysql.createConnection({
+  host: "",
+  user: "",
+  password: "",
+  database: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT Username, PasswordHash FROM USERS", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
