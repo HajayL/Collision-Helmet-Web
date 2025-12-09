93e144b8c178049dfce6a2bd5008e89b270fd1b0
HajayL <84947010+HajayL@users.noreply.github.com>
2025-12-08 22:49:15 -0700

    Adding missing stuff for the data display

--- User Data Display/canvasControl.js
+++ User Data Display/canvasControl.js
@@ -1,79 +1,207 @@
-const targetX = 1000;
-const targetY = 1000;
-
-let target = null;
-let currentCtx = null;
-
-let width = 0;
-let height = 0;
-
-let scaleX = 0;
-let scaleY = 0;
-
-function SetContext(element){
-    target = element;
-    currentCtx = target.getContext("2d");
-
-    width = target.getBoundingClientRect().width;
-    height = target.getBoundingClientRect().height;
-
-    scaleX = width / targetX;
-    scaleY = height / targetY;
-}
-
-function Clear(){
-    currentCtx.clearRect(0, 0, targetX, targetY);
-}
-
 function RGB(red, green, blue){
     return "rgb("+red+","+green+","+blue+")";
 }
 
-function SetLineThickness(thickness){
-    ctx.lineWidth = thickness;
+function RGBA(red, green, blue, alpha){
+    return "rgba("+red+","+green+","+blue+","+alpha+")";
 }
 
-function Line(startX, startY, endX, endY, color){
-    currentCtx.moveTo((startX + 500) * scaleX, (-startY + 500) * scaleY);
-    currentCtx.lineTo((endX + 500) * scaleX, (-endY + 500) * scaleY);
-    currentCtx.strokeStyle = color;
-    currentCtx.stroke();
+class Plot{
+    static TargetX = 2000;
+    static TargetY = 1000;
+
+    static FillColor = RGB(200, 200, 200);
+    static BorderColor = RGB(0, 0, 0);
+
+    dataset = [];
+    datapos = [];
+    datarange = { min: -30, max: 30 };
+    displayrange = { min: 0, max: 0 };
+    dataScale = 5;
+
+    timeData = [];
+
+    constructor(cnv){
+        let ref = this;
+
+        this.obj = cnv;
+        this.currentCtx = this.obj.getContext("2d");
+
+        this.width = this.obj.getBoundingClientRect().width;
+        this.height = this.obj.getBoundingClientRect().height;
+
+        this.scaleX = this.width / Plot.TargetX;
+        this.scaleY = this.height / Plot.TargetY;
+
+        this.docScaleX = Plot.TargetX / this.width;
+        this.docScaleY = Plot.TargetY / this.height;
+
+        cnv.addEventListener("mousemove", function(e){
+            let mousePos = { x: (e.clientX - cnv.getBoundingClientRect().left) * ref.docScaleX - Plot.TargetX / 2, y: -(e.clientY - cnv.getBoundingClientRect().top) * ref.docScaleY + Plot.TargetY / 2};
+            
+            ref.Clear();
+            ref.DrawRules();
+            ref.DrawData();
+
+            ref.CheckHover(mousePos);
+        });
+    }
+
+    Clear(){
+        this.currentCtx.clearRect(0, 0, Plot.TargetX, Plot.TargetY);
+        this.FillRect(0, 0, Plot.TargetX, Plot.TargetY, Plot.FillColor);
+        this.StrokeRect(0, 0, Plot.TargetX, Plot.TargetY, Plot.BorderColor);
+    }
+
+    SetLineThickness(thickness){
+        this.currentCtx.lineWidth = thickness;
+    }
+
+    SetFont(font){
+        this.currentCtx.font = font;
+    }
+
+    Line(startX, startY, endX, endY, color){
+        this.currentCtx.moveTo((startX + Plot.TargetX / 2) * this.scaleX, (-startY + Plot.TargetY / 2) * this.scaleY);
+        this.currentCtx.lineTo((endX + Plot.TargetX / 2) * this.scaleX, (-endY + Plot.TargetY / 2) * this.scaleY);
+        this.currentCtx.strokeStyle = color;
+        this.currentCtx.stroke();
+    }
+
+    StrokeRect(x, y, width, height, color){
+        this.currentCtx.strokeStyle = color;
+        this.currentCtx.strokeRect((x + Plot.TargetX / 2 - width / 2) * this.scaleX, (-y + Plot.TargetY / 2 - height / 2) * this.scaleY, width * this.scaleX, height * this.scaleY);
+    }
+
+    FillRect(x, y, width, height, color){
+        this.currentCtx.fillStyle = color;
+        this.currentCtx.fillRect((x + Plot.TargetX / 2 - width / 2) * this.scaleX, (-y + Plot.TargetY / 2 - height / 2) * this.scaleY, width * this.scaleX, height * this.scaleY);
+    }
+
+    StrokeCircle(x, y, radius, color){
+        this.currentCtx.beginPath();
+        this.currentCtx.strokeStyle = color;
+        this.currentCtx.arc((x + Plot.TargetX / 2) * this.scaleX, (-y + Plot.TargetY / 2) * this.scaleY, radius * this.scaleY, 0, 2 * Math.PI);
+        this.currentCtx.stroke();
+    }
+
+    FillCircle(x, y, radius, color){
+        this.currentCtx.beginPath();
+        this.currentCtx.fillStyle = color;
+        this.currentCtx.arc((x + Plot.TargetX / 2) * this.scaleX, (-y + Plot.TargetY / 2) * this.scaleY, radius * this.scaleY, 0, 2 * Math.PI);
+        this.currentCtx.fill();
+    }
+
+    StrokeText(x, y, color, text){
+        this.currentCtx.strokeStyle = color;
+        this.currentCtx.strokeText(text, (x + Plot.TargetX / 2) * this.scaleX, (-y + Plot.TargetY / 2) * this.scaleY);
+    }
+
+    FillText(x, y, color, text){
+        this.currentCtx.fillStyle = color;
+        this.currentCtx.fillText(text, (x + Plot.TargetX / 2) * this.scaleX, (-y + Plot.TargetY / 2) * this.scaleY);
+    }
+
+    SetRange(range){
+        this.datarange = range;
+    }
+
+    SetDataScale(scale){
+        this.dataScale = scale;
+    }
+
+    DrawRule(y, value){
+        this.FillRect(-Plot.TargetX / 2 + 25, y, 50, 10, RGB(0, 0, 0));
+        this.FillRect(0, y, Plot.TargetX, 10, RGBA(0, 0, 0, .1));
+        this.SetFont("12px Arial");
+        this.FillText(-Plot.TargetX / 2 + 25, y - 50, RGBA(0, 0, 0, .8), value);
+    }
+
+    DrawRules(){
+        let ruleSteps = (this.datarange.max - this.datarange.min) / this.dataScale;
+        
+        for(let i = 0; i <= ruleSteps; i++){
+            this.DrawRule((Plot.TargetY - 200) / ruleSteps * i - Plot.TargetY / 2 + 100, (i - ruleSteps / 2) * this.dataScale);
+        }
+
+        this.displayrange.min = (Plot.TargetY - 200) / ruleSteps * 0 - Plot.TargetY / 2 + 100;
+        this.displayrange.max = (Plot.TargetY - 200) / ruleSteps * ruleSteps - Plot.TargetY / 2 + 100;
+    }
+
+    DrawData(){
+        if(this.dataset.length <= 0){ return; }
+
+        let dataRange = Plot.TargetX - 150;
+        let dataSection = Math.floor(dataRange / this.dataset.length);
+
+        let drawScale = this.displayrange.min / this.datarange.min;
+        this.datapos = [];
+
+
+        for(let i = 0; i < this.dataset.length; i++){
+            this.FillCircle(dataSection * i - Plot.TargetX / 2 + 125, this.dataset[i] * drawScale, 15, RGB(0, 0, 0));
+
+            if(i > 0){
+                this.Line(dataSection * (i - 1) - Plot.TargetX / 2 + 125, this.dataset[i - 1] * drawScale, dataSection * i - Plot.TargetX / 2 + 125, this.dataset[i] * drawScale, RGB(0, 0, 0));
+            }
+
+            this.datapos[this.datapos.length] = {x: dataSection * i - Plot.TargetX / 2 + 125, y: this.dataset[i] * drawScale};
+        }
+    }
+
+    Plot(data, time = []){
+        this.dataset = data;
+        this.timeData = time;
+
+        this.Clear();
+        this.DrawRules();
+        this.DrawData();
+    }
+
+    DataTip(mousePos, index){
+        let tipSize = {x: 500, y: 200};
+        let displayDir = {x: 1, y: -1};
+
+        if(mousePos.x + tipSize.x * 1.5 * displayDir.x > Plot.TargetX / 2){ displayDir.x *= -1; }
+        if(mousePos.y + tipSize.y * 1.5 * displayDir.y < -Plot.TargetY / 2){ displayDir.y *= -1; }
+
+        this.FillRect(mousePos.x + tipSize.x * displayDir.x, mousePos.y + tipSize.y * displayDir.y, tipSize.x, tipSize.y, RGB(255, 255, 255));
+        this.StrokeRect(mousePos.x + tipSize.x * displayDir.x, mousePos.y + tipSize.y * displayDir.y, tipSize.x, tipSize.y, RGB(0, 0, 0));
+
+        this.SetFont("16px Arial");
+
+        if(displayDir.x == 1){
+            this.FillText(mousePos.x + tipSize.x * displayDir.x - tipSize.x * displayDir.x / 2 + 10, mousePos.y + tipSize.y * displayDir.y + 25, RGB(0, 0, 0), this.dataset[index]);
+            this.SetFont("12px Arial");
+            this.FillText(mousePos.x + tipSize.x * displayDir.x - tipSize.x * displayDir.x / 2 + 10, mousePos.y + tipSize.y * displayDir.y - 50, RGB(0, 0, 0), this.timeData[index]);
+        }else{
+            this.FillText(mousePos.x + tipSize.x * displayDir.x + tipSize.x * displayDir.x / 2 + 10, mousePos.y + tipSize.y * displayDir.y + 25, RGB(0, 0, 0), this.dataset[index]);
+            this.SetFont("12px Arial");
+            this.FillText(mousePos.x + tipSize.x * displayDir.x + tipSize.x * displayDir.x / 2 + 10, mousePos.y + tipSize.y * displayDir.y - 50, RGB(0, 0, 0), this.timeData[index]);
+        }
+        
+    }
+
+    CheckHover(mousePos){
+        for(let i = 0; i < this.datapos.length; i++){
+            let diff = Math.sqrt(Math.pow(mousePos.x - this.datapos[i].x, 2) + Math.pow(mousePos.y - this.datapos[i].y, 2));
+
+            if(diff <= 30){
+                this.DataTip(mousePos, i);
+            }
+        }
+    }
+
 }
 
-function StrokeRect(x, y, width, height, color){
-    currentCtx.strokeStyle = color;
-    currentCtx.strokeRect((x + 500 - width / 2) * scaleX, (-y + 500 - height / 2) * scaleY, width * scaleX, height * scaleY);
-}
+let riskGraph = new Plot(document.getElementById("risk"));
+let tempGraph = new Plot(document.getElementById("temp"));
+let accelXGraph = new Plot(document.getElementById("accelX"));
+let accelYGraph = new Plot(document.getElementById("accelY"));
+let accelZGraph = new Plot(document.getElementById("accelZ"));
+let gyroXGraph = new Plot(document.getElementById("gyroX"));
+let gyroYGraph = new Plot(document.getElementById("gyroY"));
+let gyroZGraph = new Plot(document.getElementById("gyroZ"));
 
-function FillRect(x, y, width, height, color){
-    currentCtx.fillStyle = color;
-    currentCtx.fillRect((x + 500 - width / 2) * scaleX, (-y + 500 - height / 2) * scaleY, width * scaleX, height * scaleY);
-}
-
-function StrokeCircle(x, y, radius, color){
-    currentCtx.beginPath();
-    currentCtx.strokeStyle = color;
-    currentCtx.arc((x + 500) * scaleX, (-y + 500) * scaleY, radius * scaleY, 0, 2 * Math.PI);
-    currentCtx.stroke();
-}
-
-function FillCircle(x, y, radius, color){
-    currentCtx.beginPath();
-    currentCtx.fillStyle = color;
-    currentCtx.arc((x + 500) * scaleX, (-y + 500) * scaleY, radius * scaleY, 0, 2 * Math.PI);
-    currentCtx.fill();
-}
-
-function StrokeText(x, y, color, text){
-    currentCtx.strokeStyle = color;
-    currentCtx.strokeText(text, (x + 500) * scaleX, (-y + 500) * scaleY);
-}
-
-function FillText(x, y, color, text){
-    currentCtx.fillStyle = color;
-    currentCtx.fillText(text, (x + 500) * scaleX, (-y + 500) * scaleY);
-}
-
-SetContext(document.getElementById("templateCanvas"));
-
-Clear();
+tempGraph.SetRange({min: -50, max: 50});
+tempGraph.SetDataScale(10);
--- User Data Display/script.js
+++ User Data Display/script.js
@@ -1,14 +1,18 @@
+let currentData = {};
+let lock = false;
+let riskHigh = 0;
+
+$("#dataDisplay").hide();
 
 $(document).ready(function(){
   let urlParams = new URLSearchParams(window.location.search);
    let UserID = urlParams.get('id');
 
   function fetchData() {
-    //console.log("in");
     let store = {};
    
     store = {UserID};
-    CallAJAX("https://localhost:7156/GetData", store, "POST", "html", SubmitDone, Error);
+    CallAJAX("https://localhost:7156/GetData", store, "POST", "json", SubmitDone, Error);
   } 
 
   $("#relateBtn").click(() => {
@@ -27,8 +31,49 @@ $(document).ready(function(){
     concorde.fail(fxnError);
   }
 
-  /*
-  function SubmitDone(ret){
+  
+  function SubmitDone(resp){
+
+    let data = resp.data;
+    //console.log(data);
+
+    let helmets = Object.keys(data);
+
+    let selString = "<option value='None' disabled>None</option>";
+
+    currentData = {};
+
+    for(let i = 0; i < helmets.length; i++){
+      selString += "<option value='" + helmets[i] + "'>" + helmets[i] + "</option>\n";
+
+      currentData[helmets[i]] = {accelX: [], accelY: [], accelZ: [], gyroX: [], gyroY: [], gyroZ: [], temp: [], time: []};
+    }
+
+    $("#helmetSelect").html(selString);
+
+    if(resp.success && helmets.length > 0){
+      $("#dataDisplay").show();
+
+      for(let h = 0; h < helmets.length; h++){
+        for(let i = data[helmets[h]].length - 1; i >= 0; i--){
+          currentData[helmets[h]].accelX[currentData[helmets[h]].accelX.length] = (parseInt(data[helmets[h]][i].accel.x)/1000).toFixed(2);
+          currentData[helmets[h]].accelY[currentData[helmets[h]].accelY.length] = -(parseInt(data[helmets[h]][i].accel.y)/1000).toFixed(2);
+          currentData[helmets[h]].accelZ[currentData[helmets[h]].accelZ.length] = (parseInt(data[helmets[h]][i].accel.z)/1000).toFixed(2);
+          currentData[helmets[h]].gyroX[currentData[helmets[h]].gyroX.length] = (parseInt(data[helmets[h]][i].gyro.x)/1000).toFixed(2);
+          currentData[helmets[h]].gyroY[currentData[helmets[h]].gyroY.length] = (parseInt(data[helmets[h]][i].gyro.y)/1000).toFixed(2);
+          currentData[helmets[h]].gyroZ[currentData[helmets[h]].gyroZ.length] = (parseInt(data[helmets[h]][i].gyro.z)/1000).toFixed(2);
+          currentData[helmets[h]].temp[currentData[helmets[h]].temp.length] = (parseInt(data[helmets[h]][i].temp)/10).toFixed(1);
+          currentData[helmets[h]].time[currentData[helmets[h]].time.length] = data[helmets[h]][i].time;
+        }
+      }
+      
+      DisplayData($("#helmetSelect").val());
+
+    }else{
+      $("#dataDisplay").hide();
+    }
+
+    /*
     let temp = JSON.parse(ret);
     document.getElementById("test").innerHTML = temp.userInfo[0][0];
     if(Object.keys(temp).length == 1){
@@ -65,8 +110,9 @@ $(document).ready(function(){
         $("#stat").hide();
       }
     }
+      */
   }
-  */
+  
 
   function Percent(accel, gyro) {
     const b0 = -10.2;
@@ -77,7 +123,55 @@ $(document).ready(function(){
     const z = b0 + b1 * accel + b2 * gyro + b3 * accel * gyro;
     const final = 1 / (1 + Math.exp(-z));
     return (final * 100).toFixed(2);
-  } 
+  }
 
-  //setInterval(fetchData, 500);
+  function GetRisk(index){
+    let riskList = [];
+
+    for(let i = 0; i < currentData[index].accelX.length; i++){
+      let accel = Math.sqrt(Math.pow(currentData[index].accelX[i], 2) + Math.pow(currentData[index].accelY[i], 2) + Math.pow(currentData[index].accelZ[i], 2));
+      let gyro = Math.sqrt(Math.pow(currentData[index].gyroX[i], 2) + Math.pow(currentData[index].gyroY[i], 2) + Math.pow(currentData[index].gyroZ[i], 2));
+
+      riskList[riskList.length] = Percent(accel, gyro);
+    }
+    
+    return riskList;
+  }
+
+  function DisplayData(index){
+    let risks = GetRisk(index);
+
+    let riskp = risks[risks.length - 1];
+
+    if(riskp > riskHigh){
+      riskHigh = riskp;
+    }
+
+    let risk = $("#riskDisplay");
+    risk.html(riskHigh + "%");
+
+    if(riskHigh <= 10){
+      risk.css("color", "green");
+    } 
+
+    else if(riskHigh <= 25){
+      risk.css("color", "orange");
+    }
+
+    else {
+      risk.css("color", "red");
+    }
+
+    riskGraph.Plot(risks, currentData[index].time);
+    tempGraph.Plot(currentData[index].temp, currentData[index].time);
+    accelXGraph.Plot(currentData[index].accelX, currentData[index].time);
+    accelYGraph.Plot(currentData[index].accelY, currentData[index].time);
+    accelZGraph.Plot(currentData[index].accelZ, currentData[index].time);
+    gyroXGraph.Plot(currentData[index].gyroX, currentData[index].time);
+    gyroYGraph.Plot(currentData[index].gyroY, currentData[index].time);
+    gyroZGraph.Plot(currentData[index].gyroZ, currentData[index].time);
+  }
+
+  fetchData();
+  setInterval(fetchData, 5000);
 });
